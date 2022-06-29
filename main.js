8c8b40d7b6c7285f28b7df8363783fc71ebbbf1f
seinokoula <alexandre.charles92@gmail.com>
2022-06-29 18:15:58 +0200

    remove of rem

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const cursorSpan = document.querySelector(".cursor");
 const textArray = [" au comptant", " à crédit", " en assurance-vie", " en démembrement"];
 const typingDelay = 200;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 2000;
 let textArrayIndex = 0;
 let charIndex = 0;
 
@@ -36,6 +36,6 @@ function erase() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+document.addEventListener("DOMContentLoaded", function () {
     if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
